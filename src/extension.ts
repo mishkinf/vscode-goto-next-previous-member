518f7e2831a9f2ab2b3c45986437e901a40bb78c
Mishkin Faustini <mishkinf@hotmail.com>
2018-04-23 17:04:03 -0700

    Adding in initial code to make extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,97 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let symbolIndex = 0;
+  let tree: Array<vscode.SymbolInformation> = [];
+  let dirtyTree = true;
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "goto-next-member" is now active!');
+  const refreshTree = async (editor: vscode.TextEditor) => {
+    tree = (await vscode.commands.executeCommand<vscode.SymbolInformation[]>(
+      "vscode.executeDocumentSymbolProvider",
+      editor.document.uri
+    )) || [];
+  };
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+  const documentChangeListener = vscode.workspace.onDidChangeTextDocument(e => {
+    dirtyTree = true;
+  });
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+  const setSymbolIndex = (currentLine: number, directionNext: boolean) => {
+    if(directionNext) {
+      symbolIndex = -1;
+      do {
+        symbolIndex++;
+      } while (tree[symbolIndex] && currentLine > tree[symbolIndex].location.range.start.line);
+    } else {
+      symbolIndex = tree.length;
+      do {
+        symbolIndex--;
+      } while (tree[symbolIndex] && currentLine <= tree[symbolIndex].location.range.start.line);
+    }
+  };
 
-    context.subscriptions.push(disposable);
-}
+  const previousMemberCommand = vscode.commands.registerTextEditorCommand("gotoNextPreviousMember.previousMember", async (editor: vscode.TextEditor) => {
+      let symbol;
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+      if (!tree || dirtyTree) {
+        refreshTree(editor);
+        setSymbolIndex(editor.selection.active.line, false);
+        dirtyTree = false;
+      } else {
+        if (symbolIndex >= 0) {
+          symbolIndex--;
+        } else {
+          symbolIndex = 0;
+        }
+      }
+
+      symbol = tree[symbolIndex];
+
+      if (symbol) {
+        editor.selection = new vscode.Selection(
+          symbol.location.range.start.line,
+          symbol.location.range.start.character,
+          symbol.location.range.start.line,
+          symbol.location.range.start.character
+        );
+        vscode.commands.executeCommand("revealLine", {
+          lineNumber: symbol.location.range.start.line,
+          at: "center"
+        });
+      }
+
+      console.log("Previous Member");
+    }
+  );
+
+  const nextMemberCommand = vscode.commands.registerTextEditorCommand("gotoNextPreviousMember.nextMember", async (editor: vscode.TextEditor) => {
+      let symbol;
+
+      if (!tree || dirtyTree) {
+        refreshTree(editor);
+        setSymbolIndex(editor.selection.active.line, true);
+      } else {
+        if (symbolIndex < tree.length) {
+          symbolIndex++;
+        } else {
+          symbolIndex = tree.length - 1;
+        }
+      }
+
+      symbol = tree[symbolIndex];
+
+      if (symbol) {
+        editor.selection = new vscode.Selection(symbol.location.range.start.line, symbol.location.range.start.character, symbol.location.range.start.line, symbol.location.range.start.character);
+        vscode.commands.executeCommand("revealLine", {
+          lineNumber: symbol.location.range.start.line,
+          at: "center"
+        });
+      }
+
+      console.log("Next Member");
+    }
+  );
+
+  context.subscriptions.push(previousMemberCommand, nextMemberCommand, documentChangeListener);
 }
